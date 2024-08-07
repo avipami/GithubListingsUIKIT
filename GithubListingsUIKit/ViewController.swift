a9af598db5ecffc6dfe9b7fee777b9dc89da0632
Vincent <35411050+avipami@users.noreply.github.com>
2024-08-07 11:12:37 +0200

    Initial Commit

---
+++ GithubListingsUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubListingsUIKit
+//
+//  Created by Vincent Palma on 2024-08-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
